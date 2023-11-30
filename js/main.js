cc05293df153e1dadde839d3a6f4e2490ea3ef60
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-30 23:38:27 +0600

    wordek with popular product slide

--- js/main.js
+++ js/main.js
@@ -27,3 +27,32 @@ rightSlide.addEventListener('click', () => {
 
     slider.style.transform = `translateX(-${slideIndex * 100}%)`; 
 })
+
+
+let popSlider = document.getElementsByClassName('popular-slider')[0];
+let popItems = document.querySelectorAll('.pop-item');
+
+let leftPop = document.getElementsByClassName('pop-left')[0];
+let rightPop = document.getElementsByClassName('pop-right')[0];
+
+// let slideIndex = 0;
+popItems[slideIndex].classList.add('active');
+// console.log(sliderItems);
+
+leftPop.addEventListener('click', () => {
+    popItems[slideIndex].classList.remove('active');
+
+    slideIndex = (slideIndex == 0) ? popItems.length - 1 : slideIndex - 1;
+    popItems[slideIndex].classList.add('active');
+
+    popSlider.style.transform = `translateX(-${slideIndex * 100}%)`; 
+})
+
+rightPop.addEventListener('click', () => {
+    popItems[slideIndex].classList.remove('active');
+
+    slideIndex = (slideIndex == popItems.length - 1) ? 0 : slideIndex + 1;
+    popItems[slideIndex].classList.add('active');
+
+    popSlider.style.transform = `translateX(-${slideIndex * 100}%)`; 
+})
